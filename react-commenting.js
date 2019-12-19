963a351cf7c66e645da07c0bc0fa11d962e09b48
Ubuntu <ubuntu@ip-172-31-20-253.us-east-2.compute.internal>
2019-12-19 15:26:38 +0000

    commenting

--- react-commenting.js
+++ react-commenting.js
@@ -11,7 +11,7 @@ class App extends Component{
   render(){
     return(
       <div>
-        {/* 1) Here: */}
+        {/* 1) Here: you are creating a div tags for your board to live in*/}
         <Board />
       </div>
     )
@@ -21,7 +21,7 @@ class App extends Component{
 class Board extends Component{
   constructor(){
     super()
-    // 2) Here:
+    // 2) Here: you are setting up the state of your key value pairs 
     this.state = {
       gameBoard: Array(9).fill(null),
       currentPlayer: "🦄",
@@ -30,9 +30,10 @@ class Board extends Component{
   }
 
   gamePlay = (index) => {
-    // 3) Here:
+    // 3) Here: you are destructuring 
     const { gameBoard, currentPlayer, winner, clickCount } = this.state
-    // 4) Here:
+    // 4) Here: setting you gameboards 9 spaces and the winner to have no value and the current players emoji 
+    // to the gameboard also keeping track of the click count aka turn
     if(gameBoard[index] === null && winner === null){
       gameBoard[index] = currentPlayer
       this.setState({
@@ -42,7 +43,7 @@ class Board extends Component{
       })
     }
     if(winner === null){
-      // 5) Here:
+      // 5) Here: this is giving the winning arrays 
       this.winning()
     }
   }
@@ -62,7 +63,7 @@ class Board extends Component{
     winningConditions.map(value => {
       const [a, b, c] = value
       if(gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]){
-        // 6) Here:
+        // 6) Here:setting this is setting the state of the current winner to the above winning arrays
         this.setState({
           winner: currentPlayer
         })
@@ -72,14 +73,14 @@ class Board extends Component{
 
   render(){
     const { gameBoard, currentPlayer, winner } = this.state
-    // 7) Here:
+    // 7) Here: mapping through the gameboard and render 9 squares it inherits a value and index from the array
     let mappedGameBoard = gameBoard.map((value, index) => {
       return(
         <Square
           value={ value }
           index={ index }
           key={ index }
-          {/* 8) Here: */}
+          {/* 8) Here: this. will grab the information on gameplay and update it */}
           gamePlay={ this.gamePlay }
         />
       )
@@ -89,17 +90,17 @@ class Board extends Component{
         <h1>Tic Tac Toe</h1>
 
           <div className="statusDiv">
-            {/* 9) Here: */}
+            {/* 9) Here:this updates who is the current player */}
             The Current Player is: { currentPlayer }
           </div>
 
           <div className="statusDiv">
-            {/* 10) Here: */}
+            {/* 10) Here: this updates who is the winner*/}
             The Winner is: { winner }
           </div>
 
           <div id="outcomeBoard">
-            {/* 11) Here: */}
+            {/* 11) Here: this updates whent the winner chooses 3 in a row*/}
             { mappedGameBoard }
           </div>
 
@@ -111,14 +112,14 @@ class Board extends Component{
 class Square extends Component{
 
   handleSquareClick = () => {
-    // 12) Here:
+    // 12) Here: gives the abiliy to click on a squaret
     this.props.gamePlay(this.props.index)
   }
 
   render(){
     return(
       <div id="square" onClick={ this.handleSquareClick }>
-        {/* 13) Here: */}
+        {/* 13) Here: pulls a value for each square to posses*/}
         { this.props.value }
       </div>
     )
